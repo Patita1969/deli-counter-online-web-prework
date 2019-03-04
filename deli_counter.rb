2e2b04ed9bd16b3dbfde455e41671e763d214ebf
delightful-tree-6762 <mduckworth@gmail.com>
2019-03-04 03:02:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,35 @@
 # Write your code here.
-def line(customers)
-  katz_deli = []
-  return "The line is currently empty."
+def take_a_number(current_line, new_person)
+  current_line << new_person
+  current_line.length
 end
 
-def take_a_number(array, name)
-  array << name
-  puts name, (array.index(name)+1)
+def now_serving(current_line)
+  puts "Currently serving #{current_line.first}"
+  current_line.shift
 end
+
+def line(current_line)
+  puts "The line is currently: "
+  current_line.each_with_index{|x,i| puts "#{i+1}. #{x} "}
+end
+
+katz_deli = []
+
+take_a_number(katz_deli, "Ada") #=> "1"
+take_a_number(katz_deli, "Grace") #=> "2"
+take_a_number(katz_deli, "Kent") #=> "3"
+
+line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+
+now_serving(katz_deli) #=> "Currently serving Ada"
+
+line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent"
+
+take_a_number(katz_deli, "Matz") #=> "3"
+
+line(katz_deli) #=> "The line is currently: 1. Grace 2. Kent 3. Matz"
+
+now_serving(katz_deli) #=> "Currently serving Grace"
+
+line(katz_deli) #=> "1. Kent 2. Matz"
