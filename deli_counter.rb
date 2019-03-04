6909740ba40473fbc8576adbe42f6c83c34abbb9
delightful-tree-6762 <mduckworth@gmail.com>
2019-03-04 02:53:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,14 @@
 # Write your code here.
-def line
+def line(customers)
   katz_deli = []
   return "The line is currently empty."
 end
+
+def take_a_number(array, string)
+  katz_array = []
+  puts person's name
+end
+
+
+
+  
